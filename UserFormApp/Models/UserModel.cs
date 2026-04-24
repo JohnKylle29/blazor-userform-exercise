2a23f1f2f2589595aa8dfe32c9c99858d1523180
johnkylle029 <carreonjohnkylle29@gmail.com>
2026-04-24 08:49:52 +0800

    Initial comit - Blazor UserForm exercise

---
+++ UserFormApp/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿//1st i created this
+using System.ComponentModel.DataAnnotations;
+
+namespace UserFormApp.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        public string UserAddress { get; set; }
+
+        public int? Age { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ UserFormApp/Services/EmailService.cs
@@ -0,0 +1,17 @@
+﻿//2nd i created this
+namespace UserFormApp.Services
+{
+    public class EmailService
+    {
+        public async Task<bool> IsEmailUnique(string email)
+        {
+            await Task.Delay(500); 
+
+            // fake duplicate email
+            if (email?.ToLower() == "test@gmail.com")
+                return false;
+
+            return true;
+        }
+    }
+}
